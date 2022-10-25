5ebaa5c37e13506354a769d300ebd2e407eea4b2
Istiakahmed180 <Istiakahmed180@gmail.com>
2022-10-25 11:05:51 +0600

    category fake data created

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require("./data/Categories.json");
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Assignment Project Server Running Test");
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCategory = categories.find((category) => category.id === id);
+  res.send(selectedCategory);
 });
 
 app.listen(port, () => {
